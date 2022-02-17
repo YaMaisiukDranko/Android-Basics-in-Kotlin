47700b25a55491b28b244d1c610d87f9d558f867
Yaroslav <yarikmeisan@yandex.com>
2022-02-17 22:49:51 +0300

    Create UserJava(), getEmail() in UserJava.java

--- FirstKotlin/src/Lesson2/UserJava.java
+++ FirstKotlin/src/Lesson2/UserJava.java
@@ -1,6 +1,13 @@
 public class UserJava
 {
-    
+    private final String email;
 
+    public UserJava(String email)
+    {
+        this.email = email;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 }

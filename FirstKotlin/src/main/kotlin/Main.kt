3f396ba2971552f25294568a054f8ce9e545d828
Yaroslav <yarikmeisan@yandex.com>
2022-02-17 22:35:45 +0300

    ${a + b} (in line (writte)operation)

--- FirstKotlin/src/main/kotlin/Main.kt
+++ FirstKotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>)
 {
     var name: String = "Yaroslav"
-    println("Hello World! \$name")
+    var a: Int = 2
+    var b: Int = 5
+    println("Hello World! $name")
+    println("a+b = ${a + b}")
 }

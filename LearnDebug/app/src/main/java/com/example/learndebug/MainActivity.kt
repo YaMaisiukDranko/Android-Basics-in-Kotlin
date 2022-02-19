c9db127b36f11cb3421b2c459aeec674a1a47e1b
Yaroslav <yarikmeisan@yandex.com>
2022-02-19 21:30:43 +0300

    Add TAG

--- LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
+++ LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.learndebug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    fun logging() {
+        Log.v(Companion.TAG, "Hello, world!")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }

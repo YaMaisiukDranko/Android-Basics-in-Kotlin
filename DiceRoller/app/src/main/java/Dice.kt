39c5146e6b321a2974cd8e6f2417ea37fa81f96e
Yaroslav <yarikmeisan@yandex.com>
2022-02-19 13:34:16 +0300

    Dice roll()

--- DiceRoller/app/src/main/java/Dice.kt
+++ DiceRoller/app/src/main/java/Dice.kt
@@ -1,2 +1,18 @@
 class Dice {
+    val sides = 6
+
+    fun roll(): Int
+    {
+        val randomNum = (1..6).random()
+        println(randomNum)
+        return randomNum
+    }
+}
+
+fun main ()
+{
+    val myFirstDice = Dice()
+    //println(myFirstDice.sides)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.sides} sided dice rolled ${diceRoll}!")
 }

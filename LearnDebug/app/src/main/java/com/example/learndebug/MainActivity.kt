10f9afa698ecd77c310fa9f946bb29325a79028f
Yaroslav <yarikmeisan@yandex.com>
2022-02-19 21:32:41 +0300

    Write Hello World in Log

--- LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
+++ LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
@@ -10,13 +10,15 @@ class MainActivity : AppCompatActivity() {
         Log.v(Companion.TAG, "Hello, world!")
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
     companion object {
         private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        logging()
     }
 
 }

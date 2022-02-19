1c2d5ec7fb55a0c306859a30dc979a62fd99fd2e
Yaroslav <yarikmeisan@yandex.com>
2022-02-19 21:34:10 +0300

    Add A lot of Debug Types

--- LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
+++ LearnDebug/app/src/main/java/com/example/learndebug/MainActivity.kt
@@ -7,7 +7,11 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
 
     fun logging() {
-        Log.v(Companion.TAG, "Hello, world!")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 
 

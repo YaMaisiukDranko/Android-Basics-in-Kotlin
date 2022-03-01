3eea9eb9d69fe22c98b482d70c9e085d44784d63
Yaroslav <yarikmeisan@yandex.com>
2022-03-01 18:18:55 +0300

    Type Cost Of Service in Test

--- android-basics-kotlin-tip-calculator-app-solution-main/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ android-basics-kotlin-tip-calculator-app-solution-main/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -1,6 +1,14 @@
 package com.example.tiptime
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,8 +20,9 @@ class CalculatorTests
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun calculate_20_percent_tip()
-    {
+    fun calculate_default_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
 
     }
 }

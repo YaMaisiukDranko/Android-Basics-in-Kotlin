5beb1ca3a361e8a457b7adacb9449b57baec21a8
Yaroslav <yarikmeisan@yandex.com>
2022-03-05 19:49:01 +0300

    Tests

--- android-basics-kotlin-affirmations-app-solution-main/app/src/test/java/AffirmationsListTests.kt
+++ android-basics-kotlin-affirmations-app-solution-main/app/src/test/java/AffirmationsListTests.kt
@@ -1,7 +1,14 @@
+package com.example.affirmations
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.affirmations.MainActivity
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -10,4 +17,13 @@ class AffirmationsListTests {
     @get:Rule
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
+    @Test
+    fun scroll_to_item() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9))
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
+    }
 }

1c5bb94de8fc5299f9b3d3f57d0b40cf1a32c91d
Yaroslav <yarikmeisan@yandex.com>
2022-03-05 11:26:05 +0300

    Create ItemAdapter.kt with Class Parameters

---
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+
+}

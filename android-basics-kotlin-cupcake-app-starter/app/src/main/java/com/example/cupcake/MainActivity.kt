4c1b60d964a3a46e13ff0da562fc63662e5e215b
Yaroslav <yarikmeisan@yandex.com>
2022-03-11 18:08:07 +0300

    Google Starter Code

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -17,15 +17,24 @@ package com.example.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
-{
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
 }

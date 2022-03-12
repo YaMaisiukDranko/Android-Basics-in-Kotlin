251874da8374282134c86887be7857fce5c13d36
Yaroslav <yarikmeisan@yandex.com>
2022-03-12 16:41:21 +0300

    Set up Return Arrow Button

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         setupActionBarWithNavController(navController)
     }
-    
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }

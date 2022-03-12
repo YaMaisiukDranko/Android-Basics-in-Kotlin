b1b409bf7f356934f587ca9f966afc73411673b2
Yaroslav <yarikmeisan@yandex.com>
2022-03-12 16:37:38 +0300

    Edit onCreate fun

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -26,15 +26,15 @@ import androidx.navigation.ui.setupActionBarWithNavController
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
-                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-        // Set up the action bar for use with the NavController
         setupActionBarWithNavController(navController)
     }
 }

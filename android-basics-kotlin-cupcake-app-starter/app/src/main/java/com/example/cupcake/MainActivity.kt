898fe9e773f5d675dca5b739a5a58e9dc296cc98
Yaroslav <yarikmeisan@yandex.com>
2022-03-18 23:01:47 +0300

    Paste Starter Code for Tests Chapter

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -31,13 +31,18 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        // Set up the action bar for use with the NavController
         setupActionBarWithNavController(navController)
     }
 
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -50,11 +49,6 @@ class SummaryFragment : Fragment() {
         return fragmentBinding.root
     }
 
-    fun cancelOrder() {
-        sharedViewModel.resetOrder()
-        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -69,26 +63,39 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
+        // Construct the order summary text with information from the view model
         val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
         val orderSummary = getString(
             R.string.order_details,
-            sharedViewModel.quantity.value.toString(),
+            resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
             sharedViewModel.flavor.value.toString(),
             sharedViewModel.date.value.toString(),
             sharedViewModel.price.value.toString()
         )
 
+        // Create an ACTION_SEND implicit intent with order details in the intent extras
         val intent = Intent(Intent.ACTION_SEND)
             .setType("text/plain")
             .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
             .putExtra(Intent.EXTRA_TEXT, orderSummary)
 
-            .putExtra(Intent.EXTRA_EMAIL, "gametico@yandex.com") //Email for orders
-
+        // Check if there's an app that can handle this intent before launching it
         if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            // Start a new activity with the given intent (this may open the share dialog on a
+            // device if multiple apps can handle this intent)
             startActivity(intent)
         }
+    }
 
+    /**
+     * Cancel the order and start over.
+     */
+    fun cancelOrder() {
+        // Reset order in view model
+        sharedViewModel.resetOrder()
+
+        // Navigate back to the [StartFragment] to start over
+        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
     }
 
     /**

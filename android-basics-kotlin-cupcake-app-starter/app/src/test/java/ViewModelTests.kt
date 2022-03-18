d75c214d414b5ade4bba0a027feb1654c9aacc60
Yaroslav <yarikmeisan@yandex.com>
2022-03-18 23:11:33 +0300

    Setting up @Test

--- android-basics-kotlin-cupcake-app-starter/app/src/test/java/ViewModelTests.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/test/java/ViewModelTests.kt
@@ -2,15 +2,19 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.cupcake.model.OrderViewModel
 import junit.framework.Assert.assertEquals
 import org.junit.Rule
+import org.junit.Test
+
 @get:Rule
 var instantTaskExecutorRule = InstantTaskExecutorRule()
 
 class ViewModelTests {
 
+    @Test
     fun quantity_twelve_cupcakes()
     {
         val viewModel = OrderViewModel()
         viewModel.quantity.observeForever {}
         viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
     }
 }

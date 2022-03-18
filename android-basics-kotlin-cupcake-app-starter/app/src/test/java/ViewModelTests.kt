9c4a1a5178d4b1b94f1fd09a8a4b67592074124f
Yaroslav <yarikmeisan@yandex.com>
2022-03-18 23:10:14 +0300

    Create @Rule

--- android-basics-kotlin-cupcake-app-starter/app/src/test/java/ViewModelTests.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/test/java/ViewModelTests.kt
@@ -1,2 +1,10 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import junit.framework.Assert.assertEquals
+import org.junit.Rule
+@get:Rule
+var instantTaskExecutorRule = InstantTaskExecutorRule()
+
 class ViewModelTests {
+
 }

83c813d85f1536cd3e8058db3d7a0de475e7b1a9
Yaroslav <yarikmeisan@yandex.com>
2022-03-27 20:29:19 +0300

    Some edit. With ERRORS

--- android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
+++ android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
@@ -1,9 +1,13 @@
 package com.example.android.marsphotos
 
+import coil.Coil.enqueue
 import com.example.android.marsphotos.network.MarsApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import junit.framework.Assert.*
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -15,14 +19,25 @@ class MarsApiServiceTests {
 
         service = Retrofit.Builder()
             .baseUrl(url)
-            .addConverterFactory(
-                MoshiConverterFactory.create(
+            .addConverterFactory(MoshiConverterFactory.create(
                 Moshi.Builder()
                     .add(KotlinJsonAdapterFactory())
                     .build()
             ))
             .build()
             .create(MarsApiService::class.java)
+    }
+
+    @Test
+    fun api_service()
+    {
+        enqueue("mars_photos.json")
+        runBlocking {
+            val apiResponse = service.getPhotos()
+            assertNotNull(apiResponse)
+            assertTrue("The list was empty", apiResponse.isNotEmpty())
+            assertEquals("The IDs did not match", "424905", apiResponse[0].id)
+        }
     }
 
 }

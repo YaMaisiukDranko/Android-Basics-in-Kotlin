071901889feda3461063bee1ce6d7a2ab881bbcb
Yaroslav <yarikmeisan@yandex.com>
2022-03-27 11:22:27 +0300

    Create Interface MarsApiService

--- android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,6 +1,7 @@
 package com.example.android.marsphotos.network
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
@@ -8,3 +9,8 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
+
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos()
+}

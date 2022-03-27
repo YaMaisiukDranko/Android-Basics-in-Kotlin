5d280902aa84e6303854bb5ffa5538f5eeeff439
Yaroslav <yarikmeisan@yandex.com>
2022-03-27 19:35:55 +0300

    Setting up service Retrofit Builder

--- android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
+++ android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
@@ -1,13 +1,26 @@
 package com.example.android.marsphotos
 
 import com.example.android.marsphotos.network.MarsApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MarsApiServiceTests {
     private lateinit var service: MarsApiService
     @Before
     fun setup() {
         val url = mockWebServer.url("/")
+        service = Retrofit.Builder()
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .build()
+            ))
+            .build()
+            .create(MarsApiService::class.java)
     }
 
 }

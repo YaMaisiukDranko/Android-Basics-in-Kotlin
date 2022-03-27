2c8acd49ceb8bbd5f8f4ef1dc123746d7db3ae67
Yaroslav <yarikmeisan@yandex.com>
2022-03-27 19:32:48 +0300

    Add val url in MarsApiServiceTests

--- android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
+++ android-basics-kotlin-mars-photos-app-main/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTests.kt
@@ -1,4 +1,13 @@
 package com.example.android.marsphotos
 
+import com.example.android.marsphotos.network.MarsApiService
+import org.junit.Before
+
 class MarsApiServiceTests {
+    private lateinit var service: MarsApiService
+    @Before
+    fun setup() {
+        val url = mockWebServer.url("/")
+    }
+
 }

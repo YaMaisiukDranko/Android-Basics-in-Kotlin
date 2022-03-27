d82b8254f6f072ab84c6ba1f9229044288905576
Yaroslav <yarikmeisan@yandex.com>
2022-03-27 20:40:06 +0300

    Create Debugging Project with Google MainActivity Code

---
+++ Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.debugging.R
+
+public val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+}

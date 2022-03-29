f1222308b66535203b6bf7d799f97f696ea57304
Yaroslav <yarikmeisan@yandex.com>
2022-03-29 21:07:16 +0300

    Create ScheduleDAO Interface

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -1,4 +1,4 @@
-package com.example.busschedule.database
+package com.example.busschedule.database.schedule
 
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
---
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/schedule/ScheduleDAO.kt
@@ -0,0 +1,7 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.Dao
+
+@Dao
+interface ScheduleDAO {
+}

ad0b8c96db93923ecf6a790c98e4065860427960
Yaroslav <yarikmeisan@yandex.com>
2022-03-30 16:26:14 +0300

    Create fun getDatabase in companionObject

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -1,5 +1,7 @@
 package com.example.busschedule.database
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.busschedule.database.schedule.ScheduleDAO
 
@@ -9,5 +11,19 @@ abstract class AppDatabase : RoomDatabase {
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database")
+                    .createFromAsset("database/bus_schedule.db")
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
     }
 }

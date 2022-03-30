0943df79eb9160b5ac97fe852da7f80e16b27edf
Yaroslav <yarikmeisan@yandex.com>
2022-03-30 16:24:00 +0300

    Add abstract fun scheduleDAO

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -1,7 +1,9 @@
 package com.example.busschedule.database
 
 import androidx.room.RoomDatabase
+import com.example.busschedule.database.schedule.ScheduleDAO
 
 abstract class AppDatabase : RoomDatabase {
+    abstract fun scheduleDao(): ScheduleDAO
     
 }

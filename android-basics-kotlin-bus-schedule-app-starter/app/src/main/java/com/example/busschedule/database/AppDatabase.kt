5f70cbc722494eb9268a26fe6bc794b21bebdb65
Yaroslav <yarikmeisan@yandex.com>
2022-03-30 16:28:20 +0300

    Add @Database to abstract class

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -1,11 +1,14 @@
 package com.example.busschedule.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDAO
 
-abstract class AppDatabase : RoomDatabase {
+@Database(entities = arrayOf(Schedule::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
     abstract fun scheduleDao(): ScheduleDAO
 
     companion object {

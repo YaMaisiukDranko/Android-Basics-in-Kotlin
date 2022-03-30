058e338b48a16e88c3d8e8347ffae92b4901827e
Yaroslav <yarikmeisan@yandex.com>
2022-03-30 09:36:06 +0300

    Create fun fullSchedule, scheduleForStopName

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.busschedule.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDAO
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDAO): ViewModel() {
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
 }

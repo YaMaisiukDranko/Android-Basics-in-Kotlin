398aa4e067dcafd26fce7541677f531718a6b490
Yaroslav <yarikmeisan@yandex.com>
2022-04-09 16:05:45 +0300

    Edit val BusStopAdapter

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.busschedule
 
 import android.os.Bundle
@@ -21,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.FullScheduleFragmentBinding
 import com.example.busschedule.viewmodels.BusScheduleViewModel
 import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class FullScheduleFragment: Fragment() {
 
@@ -35,19 +23,12 @@ class FullScheduleFragment: Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
+
     private val viewModel: BusScheduleViewModel by activityViewModels {
         BusScheduleViewModelFactory(
             (activity?.application as BusScheduleApplication).database.scheduleDao()
         )
     }
-
-    val busStopAdapter = BusStopAdapter({
-        val action = FullScheduleFragmentDirections.actionFullScheduleFragmentToStopScheduleFragment(
-            stopName = it.stopName
-        )
-        view.findNavController().navigate(action)
-    })
-    recyclerView.adapter = busStopAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,8 +44,20 @@ class FullScheduleFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val busStopAdapter = BusStopAdapter({
+            val action = FullScheduleFragmentDirections
+                .actionFullScheduleFragmentToStopScheduleFragment(
+                    stopName = it.stopName
+                )
+            view.findNavController().navigate(action)
+        })
+        recyclerView.adapter = busStopAdapter
+        lifecycle.coroutineScope.launch {
+            viewModel.fullSchedule().collect() {
+                busStopAdapter.submitList(it)
+            }
+        }
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()

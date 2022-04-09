34f841fdfa9f584eddc8d80e7d56e0521aa08261
Yaroslav <yarikmeisan@yandex.com>
2022-04-09 14:02:28 +0300

    Create class BusStopViewHolder in BusStopAdapter

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/BusStopAdapter.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -1,5 +1,21 @@
+import android.annotation.SuppressLint
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+}
+
+class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+    @SuppressLint("SimpleDateFormat")
+    fun bind(schedule: Schedule) {
+        binding.stopNameTextView.text = schedule.stopName
+        binding.arrivalTimeTextView.text = SimpleDateFormat(
+            "h:mm a").format(
+            Date(schedule.arrivalTime.toLong() * 1000)
+        )
+    }
 }

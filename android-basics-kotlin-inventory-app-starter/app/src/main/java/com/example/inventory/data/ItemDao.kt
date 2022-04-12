3f6f1511e3d4f41d5aaa42ae58851ff287bb4fbf
Yaroslav <yarikmeisan@yandex.com>
2022-04-12 07:58:27 +0300

    Add @Insert to interface

--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,8 +1,11 @@
 package com.example.inventory.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface ItemDao {
-    
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
 }

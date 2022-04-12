e1556e37d57819d2c2f011bd83bd099c7e856a0e
Yaroslav <yarikmeisan@yandex.com>
2022-04-12 09:06:27 +0300

    Add @Update, @Delete

--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,11 +1,13 @@
 package com.example.inventory.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
 
 @Dao
 interface ItemDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
+    @Update
+    suspend fun update(item: Item)
+    @Delete
+    suspend fun delete(item: Item)
 }

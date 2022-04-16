f6ae7972a2dd041dcc0c499bab5c1464e32375a9
Yaroslav <yarikmeisan@yandex.com>
2022-04-16 22:14:30 +0300

    Add extension function called Item.getFormattedPrice() to Item.kt

--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
@@ -18,10 +18,13 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 /**
  * Entity data class represents a single row in the database.
  */
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
 @Entity
 data class Item(
     @PrimaryKey(autoGenerate = true)

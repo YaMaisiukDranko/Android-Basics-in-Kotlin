04b282b4229d1fac92e017c24231e369f1a6989c
Yaroslav <yarikmeisan@yandex.com>
2022-04-17 12:54:02 +0300

    Solution Google code

--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/ItemListAdapter.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/ItemListAdapter.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.inventory
 
 import android.view.LayoutInflater
@@ -38,14 +53,10 @@ class ItemListAdapter(private val onItemClicked: (Item) -> Unit) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Item) {
-            binding.apply {
-                itemName.text = item.itemName
-                itemPrice.text = item.getFormattedPrice()
-                itemQuantity.text = item.quantityInStock.toString()
-            }
+            binding.itemName.text = item.itemName
+            binding.itemPrice.text = item.getFormattedPrice()
+            binding.itemQuantity.text = item.quantityInStock.toString()
         }
-
-
     }
 
     companion object {
--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
@@ -23,8 +23,6 @@ import java.text.NumberFormat
 /**
  * Entity data class represents a single row in the database.
  */
-fun Item.getFormattedPrice(): String =
-    NumberFormat.getCurrencyInstance().format(itemPrice)
 @Entity
 data class Item(
     @PrimaryKey(autoGenerate = true)
@@ -36,3 +34,8 @@ data class Item(
     @ColumnInfo(name = "quantity")
     val quantityInStock: Int,
 )
+/**
+ * Returns the passed in price in currency format.
+ */
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)

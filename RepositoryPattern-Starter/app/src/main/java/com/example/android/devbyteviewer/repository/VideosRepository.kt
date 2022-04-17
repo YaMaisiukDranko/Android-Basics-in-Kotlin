ce45fdd80e33158e59061eddfe476573058bdb72
Yaroslav <yarikmeisan@yandex.com>
2022-04-17 15:57:33 +0300

    Create suspend fun refreshVideos inside VideosRepository class

--- RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,5 +16,18 @@
 
 package com.example.android.devbyteviewer.repository
 
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class VideosRepository(private val database: VideosDatabase) {
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = DevByteNetwork.devbytes.getPlaylist()
+            database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
 }

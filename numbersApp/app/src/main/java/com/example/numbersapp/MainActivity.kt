bc669b1dac8f73b448964f9f24201075be21e20d
Yaroslav <yarikmeisan@yandex.com>
2022-05-01 23:20:20 +0300

    Add val initialTextViewTranslationY

--- numbersApp/app/src/main/java/com/example/numbersapp/MainActivity.kt
+++ numbersApp/app/src/main/java/com/example/numbersapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.numbersapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val initialTextViewTranslationY = textViewProgress.translationY
     }
+
 }

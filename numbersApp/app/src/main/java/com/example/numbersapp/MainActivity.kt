22ad5fd7f8e86f5e555afc059093259317bc375d
Yaroslav <yarikmeisan@yandex.com>
2022-05-02 13:54:20 +0300

    Set up app

--- numbersApp/app/src/main/java/com/example/numbersapp/MainActivity.kt
+++ numbersApp/app/src/main/java/com/example/numbersapp/MainActivity.kt
@@ -1,14 +1,45 @@
 package com.example.numbersapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val initialTextViewTranslationY = textViewProgress.translationY
-    }
 
+        val initialTextViewTranslationY = textViewProgress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textViewProgress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY +
+                        progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textViewProgress.animate().translationY(translationDistance)
+                if (!fromUser)
+                    textViewProgress.animate().setDuration(500).rotationBy(360f)
+                        .translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        resetButton.setOnClickListener { v ->
+            seekBar.progress = 0
+        }
+    }
 }
